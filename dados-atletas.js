e6afd33373b4e5e6e9338178de103aea9869c2af
Gabriela Lisboa <gabinblisboa@gmail.com>
2026-03-08 23:21:43 -0300

    Exibe resultados

--- dados-atletas.js
+++ dados-atletas.js
@@ -52,6 +52,10 @@ class Atleta {
     return this.peso;
   };
 
+  obtemAltura() {
+    return this.altura
+  }
+
   obtemNotasAtleta() {
     return this.notas;
   };
@@ -70,4 +74,23 @@ class Atleta {
 const atleta = new Atleta("Cesar Abascal",
     30, 80, 1.70,
     [10, 9.34, 8.42, 10, 7.88]);
+
+    console.log(atleta);
+    
+
+// Exibe os resultados
+console.log(`Nome: ${atleta.obtemNomeAtleta()}`);
+console.log(`Idade: ${atleta.obtemIdadeAtleta()}`);
+console.log(`Peso: ${atleta.obtemPesoAtleta()}`);
+console.log(`Altura: ${atleta.obtemAltura()}`);
+console.log(`Notas: ${atleta.obtemNotasAtleta()}`);
+console.log(`Categoria: ${atleta.obtemCategoria()}`);
+console.log(`IMC: ${atleta.obtemIMC()}`);
+console.log(`Media válida: ${atleta.obtemMediaValida()}`);
+
+
+
+
+
+
 
