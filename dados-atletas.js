02c51b4fd9f806e574a7482abac742cc5a8daceb
Gabriela Lisboa <gabinblisboa@gmail.com>
2026-03-08 23:05:45 -0300

    Implementacao dos metodos de obtencao de nome, idade e peso

--- dados-atletas.js
+++ dados-atletas.js
@@ -8,46 +8,54 @@ class Atleta {
   }
   calculaCategoria() {
     if (this.idade >= 9 && this.idade <= 11) {
-      return "Categoria: Infantil";
+      return "Infantil";
     } else if (this.idade >= 12 && this.idade <= 13) {
-      return "Categoria: Juvenil";
+      return "Juvenil";
     } else if (this.idade >= 14 && this.idade <= 15) {
-      return "Categoria: Intermediário";
+      return "Intermediário";
     } else if (this.idade >= 16 && this.idade <= 30) {
-      return "Categoria: Adulto";
+      return "Adulto";
     } else {
-      return "Categoria: Sem categoria";
-    };
+      return "Sem categoria";
+    }
   };
   calculaIMC() {
     let imc = this.peso / Math.pow(this.altura, 2);
     return `IMC: ${imc}`;
   };
   calculaMediaValida() {
-    
-  }
+    //Ordenando as notas do atleta
+    this.notas = this.notas.sort((a, b) => a - b);
+
+    //Tirando a primeira e a última notas do array de notas ordenado
+    let notasValidas = this.notas.slice(1, 4);
+
+    //Somando as notas
+    let soma = 0;
+    notasValidas.forEach(function (nota) {
+      soma = soma + nota;
+      return soma;
+    });
+
+    // Calculando a média das notas de cada atleta
+    let media = soma / notasValidas.length;
+    return media;
+  };
   obtemNomeAtleta() {
-
+    return this.nome
   }
   obtemIdadeAtleta() {
-
+    return this.idade
   }
-  obtemIdadeAtleta() {
-
-  }
+  
   obtemPesoAtleta() {
-
+    return this.peso
   }
+
   obtemNotasAtleta() {
-
+    return this.notas
   }
-  obtemCategoria() {
-
-  }
-  obtemIMC() {
-
-  }
-  obtemMediaValida() {
-
-  }
+  obtemCategoria() {}
+  obtemIMC() {}
+  obtemMediaValida() {}
 }
