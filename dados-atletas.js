511d94f5d02373cb288760fce3ba9e0e5ed3456a
Gabriela Lisboa <gabinblisboa@gmail.com>
2026-03-08 22:17:59 -0300

    Metodo calculaCategoria() da classe Atleta
    Implementacao do metodo calculaCategoria()

--- dados-atletas.js
+++ dados-atletas.js
@@ -7,8 +7,18 @@ class Atleta {
     this.notas = notas;
   }
   calculaCategoria() {
-    
-  }
+    if (this.idade >= 9 && this.idade <= 11) {
+      return "Categoria: Infantil";
+    } else if (this.idade >= 12 && this.idade <= 13) {
+      return "Categoria: Juvenil";
+    } else if (this.idade >= 14 && this.idade <= 15) {
+      return "Categoria: Intermediário";
+    } else if (this.idade >= 16 && this.idade <= 30) {
+      return "Categoria: Adulto";
+    } else {
+      return "Categoria: Sem categoria";
+    };
+  };
   calculaIMC() {
 
   }
